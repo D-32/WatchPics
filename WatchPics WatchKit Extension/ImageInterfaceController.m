5bf1f6d886564a6fff536f5ac9606e0ae75b10f0
Dylan Marriott <info@d-32.com>
2015-01-11 03:26:35 +0100

    "Like" prototype

--- WatchPics WatchKit Extension/ImageInterfaceController.m
+++ WatchPics WatchKit Extension/ImageInterfaceController.m
@@ -9,17 +9,21 @@
 #import "ImageInterfaceController.h"
 #import "ImageStore.h"
 #import "ImageInfo.h"
+#import "OverlayRowController.h"
 
 @interface ImageInterfaceController()
 @property (weak, nonatomic) IBOutlet WKInterfaceGroup *background;
 @property (weak, nonatomic) IBOutlet WKInterfaceLabel *nameLabel;
 @property (weak, nonatomic) IBOutlet WKInterfaceImage *profileImage;
+@property (weak, nonatomic) IBOutlet WKInterfaceTable *table;
 
 
 @end
 
 
-@implementation ImageInterfaceController
+@implementation ImageInterfaceController {
+    NSDate* _lastTouch;
+}
 
 - (void)awakeWithContext:(id)context {
     [super awakeWithContext:context];
@@ -28,6 +32,18 @@
     [self.background setBackgroundImage:imageInfo.image];
     [self.nameLabel setText:imageInfo.name];
     [self.profileImage setImage:imageInfo.profileImage];
+    
+    [self.table setNumberOfRows:1 withRowType:@"row"];
+}
+
+- (void)table:(WKInterfaceTable *)table didSelectRowAtIndex:(NSInteger)rowIndex {
+    OverlayRowController *rowController = [table rowControllerAtIndex:rowIndex];
+    if (_lastTouch && [[NSDate date] timeIntervalSinceDate:_lastTouch] < 0.4) {
+        [rowController.icon setAlpha:1.0];
+    } else {
+        [rowController.icon setAlpha:0.0];
+    }
+    _lastTouch = [NSDate date];
 }
 
 - (void)willActivate {
---
+++ WatchPics WatchKit Extension/OverlayRowController.h
@@ -0,0 +1,16 @@
+//
+//  OverlayRowController.h
+//  WatchPics
+//
+//  Created by Dylan Marriott on 11/01/15.
+//  Copyright (c) 2015 Dylan Marriott. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <WatchKit/WatchKit.h>
+
+@interface OverlayRowController : NSObject
+
+@property (weak, nonatomic) IBOutlet WKInterfaceImage *icon;
+
+@end
